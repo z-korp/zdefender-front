6e654457c920b2c32850ccb44466f44eed135ce4
Matthias Monnier <matthias.monnier@gmail.com>
2023-10-22 17:02:18 +0200

    add images

--- src/assets/mobs/mobs.ts
+++ src/assets/mobs/mobs.ts
@@ -1,14 +1,40 @@
 import { MobType } from '@/utils/wave';
 import * as PIXI from 'pixi.js';
 
+import blackdragonImage from './blackdragon.png';
 import boarImage from './boar.png';
+import borgImage from './borg.png';
 import chickenImage from './chicken.png';
+import grumImage from './grum.png';
+import katanImage from './katan.png';
 import mammothImage from './mammoth.png';
+import minotaurImage from './minotaur.png';
+import necromancerImage from './necromancer.png';
+import orcImage from './orc.png';
+import orcshamanImage from './orcshaman.png';
+import reddemonImage from './reddemon.png';
+import reddemonarmoredImage from './reddemonarmored.png';
+import speargoblinImage from './speargoblin.png';
+import wendigoImage from './wendigo.png';
+import yetiImage from './yeti.png';
 
 export const MobImages: Record<MobType, string> = {
   [MobType.CHICKEN]: chickenImage,
   [MobType.MAMMOTH]: mammothImage,
   [MobType.BOAR]: boarImage,
+  [MobType.WENDIGO]: wendigoImage,
+  [MobType.YETI]: yetiImage,
+  [MobType.BORG]: borgImage,
+  [MobType.RED_DEMON]: reddemonImage,
+  [MobType.RED_DEMON_ARMORED]: reddemonarmoredImage,
+  [MobType.NECROMANCER]: necromancerImage,
+  [MobType.MINOTAUR]: minotaurImage,
+  [MobType.ORC]: orcImage,
+  [MobType.SPEAR_OGOBLIN]: speargoblinImage,
+  [MobType.ORC_SHAMAN]: orcshamanImage,
+  [MobType.GRUM]: grumImage,
+  [MobType.KATAN]: katanImage,
+  [MobType.DRAGON]: blackdragonImage,
 };
 
 PIXI.Texture.from(MobImages[MobType.CHICKEN]).baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
